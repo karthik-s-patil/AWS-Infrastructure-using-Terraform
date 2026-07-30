cb6da71cd86fcbe036d0e02c07400bbb955c4750
Karthik <karthikspatil2004@gmail.com>
2026-07-30 11:16:45 +0530

    Update asg_ec2.tf

--- terraform-production-infra/asg_ec2.tf
+++ terraform-production-infra/asg_ec2.tf
@@ -23,3 +23,28 @@ resource "aws_launch_template" "web_lt" {
   }
 }
 
+# Auto Scaling Group
+resource "aws_autoscaling_group" "web_asg" {
+  name             = "web-asg"
+  min_size         = 2
+  desired_capacity = 2
+  max_size         = 4
+
+  vpc_zone_identifier = [
+    aws_subnet.pr_sbnt_1.id,
+    aws_subnet.pr_sbnt_2.id
+  ]
+
+  target_group_arns = [
+    aws_lb_target_group.web_tg.arn
+  ]
+
+  launch_template {
+    id      = aws_launch_template.web_lt.id
+    version = "$Latest"
+  }
+
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+}
+
