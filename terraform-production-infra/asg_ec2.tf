53ed94dad5c2f48490d5d7f4535b7fbd37a9b537
Karthik <karthikspatil2004@gmail.com>
2026-07-30 13:32:00 +0530

    Update asg_ec2.tf

--- terraform-production-infra/asg_ec2.tf
+++ terraform-production-infra/asg_ec2.tf
@@ -10,6 +10,18 @@ resource "aws_launch_template" "web_lt" {
     aws_security_group.ec2_sg.id
   ]
 
+  user_data = base64encode(<<-EOF
+   #!/bin/bash
+   apt-get update -y
+   apt-get install -y nginx
+
+  systemctl enable nginx
+  systemctl start nginx
+
+   echo "<h1>Welcome from Auto Scaling Group</h1>" > /var/www/html/index.html
+  EOF
+  )
+
   tag_specifications {
     resource_type = "instance"
 
